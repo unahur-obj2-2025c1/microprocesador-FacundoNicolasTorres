663220459ec851b33e16c3fd56f31323441bf6dd
FacundoNicolasTorres <facujaritor@gmail.com>
2025-06-03 17:03:06 -0300

    iniciando con el microcontrolador

--- src/main/java/ar/edu/unahur/obj2/command/Microprocesador.java
+++ src/main/java/ar/edu/unahur/obj2/command/Microprocesador.java
@@ -1,5 +1,78 @@
 package ar.edu.unahur.obj2.command;
 
-public class Microprocesador {
+import java.util.List;
+
+import ar.edu.unahur.obj2.command.comandos.Operable;
+
+public class Microprocesador implements Programable{
+    private Integer acumuladorA;
+    private Integer acumuladorB;
+    private Integer programCounter;
+
+    public Microprocesador(Integer acumuladorA, Integer acumuladorB, Integer programCounter){
+        this.acumuladorA = acumuladorA;
+        this.acumuladorB = acumuladorB;
+        this.programCounter = programCounter;
+    }
+
+    @Override
+    public void run(List<Operable> operaciones) {
+
+    }
+
+    @Override
+    public void incProgramCounter() {
+        programCounter++;
+    }
+
+    @Override
+    public Integer getProgramCounter() {
+        return programCounter;
+    }
+
+    @Override
+    public void setAcumuladorA(Integer value) {
+        acumuladorA = value;
+    }
+
+    @Override
+    public Integer getAcumuladorA() {
+        return acumuladorA;
+    }
+
+    @Override
+    public void setAcumuladorB(Integer value) {
+        acumuladorB = value;
+    }
+
+    @Override
+    public Integer getAcumuladorB() {
+        return acumuladorB;
+    }
+
+    @Override
+    public void copyFrom(Programable programable) {
+
+    }
+
+    @Override
+    public Programable copy() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setAddr(Integer addr) {
+
+    }
+
+    @Override
+    public Integer getAddr(Integer addr) {
+
+    }
 
 }
